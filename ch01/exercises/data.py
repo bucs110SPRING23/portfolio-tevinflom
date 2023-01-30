f0b7c8f5664ef8642767fa9e2e21fe27adf652db
Tevin Flom <tflom1@binghamton.edu>
2023-01-30 17:13:11 -0500

    lets see

---
+++ ch01/exercises/data.py
@@ -0,0 +1,11 @@
+print(10*5)
+print(10 ** 2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print (10%10)
+print (0%10)
+print (10/15)
+#the output of the last one is a float but the inputs are ints
