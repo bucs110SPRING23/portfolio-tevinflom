d294c31cb05384bbbd4992bcff3a927cac4793c4
Tevin Flom <tflom1@binghamton.edu>
2023-02-01 16:53:53 -0500

    my colors

--- ch01/exercises/data.py
+++ ch01/exercises/data.py
@@ -8,4 +8,4 @@ print(10%15)
 print (10%10)
 print (0%10)
 print (10/15)
-#the output of the last one is a float but the inputs are ints
+#the output of the last one is a float but the inputs are ints, it gives a repeating decimal 
