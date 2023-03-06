03481a84809526317511218868ba9e57ad277334
Tevin Flom <tevinflom@Tevins-MacBook-Pro.local>
2023-03-06 17:06:59 -0500

    get

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,10 +1,36 @@
 import turtle
-import pygame 
 import random
 
-pygame.init()
+pen = turtle.Turtle() 
+pen.shape("turtle")
+pen.speed(0)
 
-#pen = turtle.Turtle() 
-screen = pygame.display.set_mode()
-dimensions = screen.get_size()
-print(dimensions)
+distance = 10 
+angle = 90
+is_in_screen = True
+
+colors = ["red", "orange", "yellow", "green"]
+
+while is_in_screen: 
+    coin = random.randrange(0,2)
+    if coin:
+        turtle.right(angle)
+    else:
+        turtle.left(angle)
+    turtle.forward(distance)
+
+    turtleX = turtle.xcor()
+    turtleY = turtle.ycor()
+
+    x_range = wn.window_width() / 2
+    y_range = wn.window.width() / 2
+
+    turtle.color(random.choice(colors))
+    if abs(turtleX) > x_range or abs(turtleY) > y_range:
+        is_in_screen = False 
+
+
+
+#print(dimensions)
+#center of screen = (896,560)
+
