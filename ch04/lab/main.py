42c77a8cc8aa0ebbed5b86531635681ea11a9eaa
Tevin Flom <tevinflom@Tevins-MacBook-Pro.local>
2023-03-06 23:51:15 -0500

    get in

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -9,23 +9,8 @@ screen = pygame.display.set_mode()
 dimensions = screen.get_size() 
 center = (dimensions [0] // 2, dimensions [1] // 2)
 
-#xcor1 = random.randrange(350)
-#ycor1 = random.randrange(350)
-#xcor2 = random.randrange(350)
-#ycor2 = random.randrange(350)
-
-
-
 pygame.draw.circle(screen, "pink", center, 300)
 pygame.display.flip()
-#pygame.time.wait(30000)
-
-
-#player1 = pygame.draw.circle(screen, "red", (xcor1, ycor1), 10)
-#player2 = pygame.draw.circle(screen, "blue", (xcor2, ycor2), 10)
-#darts = (player1, player2)
-#p1points = []
-#p2points = []
 
 
 for i in range(10):
@@ -37,8 +22,8 @@ for i in range(10):
     player1 = pygame.draw.circle(screen, "red", (xcor1, ycor1), 10)
     player2 = pygame.draw.circle(screen, "blue", (xcor2, ycor2), 10)
     darts = (player1, player2)
-    p1points = []
-    p2points = []
+    p1points = [0]
+    p2points = [0]
     for player1 in darts:
         pygame.draw.circle(screen, "red", (xcor1, ycor1), 10)
         pygame.display.flip()
@@ -57,29 +42,28 @@ for i in range(10):
             print("player 2 good shot")
         else: 
             print("player 2 missed")
-if sum(p1points) > sum(p2points):
+if len(p1points) > len(p2points):
     message = "Red Player 1 Wins!"
-elif sum(p1points) == sum(p2points):
+elif len(p1points) == len(p2points):
     message = "TIE!" 
 else:
     message = "Blue Player 2 Wins!"
 
+import pygame
+pygame.init()
 font = pygame.font.Font(None, 48) 
 text = font.render(message, True, "white") 
 screen.blit(text, (300,300))
+pygame.display.flip()
+pygame.time.wait (5000)
 
 
-
-#for _ in range(10):
-    #for player1 in darts:
-        #if xcor <= 300  and ycor <= 300:
-            #p1points.append(1)
-        #else:
-            #break 
-    #for player2 in darts:
-       # if xcor <= 300 and ycor <= 300:
-            #p1points.append(1)
-       #else:
-            #break 
-
-
+while True:
+    pygame.draw.rect(screen,"red",[100,200,100,200])
+    pygame.draw.rect(screen, "blue", [200, 300, 100, 200])
+    redbutton = pygame.draw.rect(screen,"red",[100,200,100,200])
+    bluebutton = pygame.draw.rect(screen, "blue", [200, 300, 100, 200])
+    if pygame.MOUSEBUTTONDOWN(redbutton) and len(p1points) > len(p2points):
+        print("you won the bet")
+    else:
+        print("you lost") 
