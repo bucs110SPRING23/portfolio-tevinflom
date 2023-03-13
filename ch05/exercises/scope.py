0c20b8c01becebda2ba369eb7a2bdfadf8337a7c
Tevin Flom <tevinflom@Tevins-MacBook-Pro.local>
2023-03-13 18:09:38 -0400

    grapes

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,12 @@
+#param1 = int(input("input an int"))
+#param2 = int(input("input another int"))
+param3 = int(input("input an int to square"))
+
+def multiplier (param3, param3): 
+    for i in range(param3): 
+        return param3 + param3 
+def exponent (param3, param3): 
+    for i in range(param3): 
+        return param3* param3
+def square(1):
+    print(exponent(param3, param3))
