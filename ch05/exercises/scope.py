d121c30f01f348a6c4216bcd59addc68ad654d0b
Tevin Flom <tevinflom@Tevins-MacBook-Pro.local>
2023-03-15 17:07:01 -0400

    sus

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -1,12 +1,27 @@
-#param1 = int(input("input an int"))
-#param2 = int(input("input another int"))
-param3 = int(input("input an int to square"))
 
-def multiplier (param3, param3): 
-    for i in range(param3): 
-        return param3 + param3 
-def exponent (param3, param3): 
-    for i in range(param3): 
-        return param3* param3
-def square(1):
-    print(exponent(param3, param3))
+def multiplier (x,y): 
+    accumulator = 0 
+    for i in range(y): 
+        accumulator = accumulator + x
+    return accumulator 
+
+def exponent (x,y): 
+    accumulator = 1
+    for i in range(y): 
+        accumulator = accumulator * x
+    return accumulator
+    
+def square(x):
+    return multiplier(x, x) # x = x, y = x
+
+def main():
+    x = int(input("enter a number: "))
+    y = int(input("enter a number: "))
+    result = multiplier (x, y) 
+    print(result) 
+    result = exponent (x, y) 
+    print(result) 
+    result = square (x, y) 
+    print(result) 
+
+main()
