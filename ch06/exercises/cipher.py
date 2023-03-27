64fde2e61781463d9f6f3a17a0206c253bc9a692
Tevin Flom <tevinflom@Tevins-MacBook-Pro.local>
2023-03-27 16:43:45 -0400

    j

--- ch06/exercises/cipher.py
+++ ch06/exercises/cipher.py
@@ -2,6 +2,7 @@ import json
 shift = 11
 text = "the quick brown fox jumped over the lazy dog"
 results = ""
+#file_pointer= open("encript.txt", "x")
 def caeser_cipher(text, shift):
 
     """
@@ -18,25 +19,26 @@ def caeser_cipher(text, shift):
         # Determine the case of the character
             start = ord('A') if char.isupper() else ord('a')
         # Calculate the new position of the character after the shift
-            new_pos = (ord(char) - start + shift) % 26
+            new_pos = (ord(char) - start * shift) % 26
             # Convert the new position back to a character
             char = chr(start + new_pos)
             result += char
-            results.append(result)
-            print(results)
+            #results.append(result)
+            #print(results)
     return results
 
 def main():
-   file_pointer = open("ch06/exercises/encripted.txt", "w")
+   file_pointer = open("ch06\exercises\encripted.txt", "w")
    result = caeser_cipher(text, shift)
    results = []
    results.append(result)
    for i in results:
        file_pointer.write(i)
        file_pointer.close()
+       print('done')
        return None
 
 
 main()
-caeser_cipher("get a hat", shift)
+#caeser_cipher(text, shift)
 
