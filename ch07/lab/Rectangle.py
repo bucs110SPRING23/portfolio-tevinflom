e1b174a8a4230a57e55f52c17c02ba66c00f9c4e
Tevin Flom <tevinflom@Tevins-MacBook-Pro.local>
2023-03-30 18:53:01 -0400

    finally done

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -6,5 +6,10 @@ class Rectangle:
         self.width = abs(w)
         return None
     def __str__(self): 
-        myoutput = print("(x: {self.x}, y: {self.y}), width: {self.width}, height: {self.height}")
+        """ 
+        This funtion takes the instance variables from init and formats them as a string 
+        args: (Self) the function calls itself to return the characteristics of the rectangle object 
+        return: (str) the function returns the x, y, width, and height of the rectangle object as a string 
+        """
+        myoutput = ("(x: {self.x}, y: {self.y}), width: {self.width}, height: {self.height}")
         return myoutput 
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -5,5 +5,10 @@ class Surface:
         self.image = str(filename) 
         self.rect = Rectangle(x, y, h, w)
     def getRect(self):
+        """
+        This function returns the self.rect object whihc is a rectangle with the attributes x, y, width (w), and height(h)
+        args: (self) the function calls itself to get data from the init function before it 
+        return: (Rectangle object) this returns the rectangle object from line 6 
+        """
         return self.rect 
     
